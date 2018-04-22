9f4d478413775c5235ae366b74052e1040f86de7
Wallerstein, Ilan (iw4360) <iw4360@intl.att.com>
2018-04-22 19:23:08 +0300

    update

--- src/test/java/att/GroupOfChainsTests/AddDataTest.java
+++
@@ -1,7 +0,0 @@
-package att.GroupOfChainsTests;
-
-import junit.framework.TestCase;
-
-public class AddDataTest extends TestCase {
-
-}
---
+++ src/test/java/att/groupofchainstests/ChainsTests.java
@@ -0,0 +1,13 @@
+package att.groupofchainstests;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChainsTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
