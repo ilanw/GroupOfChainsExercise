b37c184fb20d727f2db3d90bef056ed35e99996d
Wallerstein, Ilan (iw4360) <iw4360@intl.att.com>
2018-04-23 14:45:45 +0300

    update

--- src/test/java/att/groupofchainstests/ChainsTests.java
+++ src/test/java/att/groupofchainstests/ChainsTests.java
@@ -1,7 +1,10 @@
 package att.groupofchainstests;
+
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.Test;
@@ -10,17 +13,28 @@ import com.att.biq.db.Chains;
 import com.att.biq.db.DbConnection;
 import com.mysql.jdbc.Connection;
 
-public class ChainsTests {
-	
-	
+public class ChainsTests extends DbConnection {
+
+	public ChainsTests() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	@Test
-	public void CreateChainGoodTest() throws SQLException {
-		
-	//	boolean res = new Chains(con).createChain("Test", null, "Vandaly ind.");
-//		assertTrue(res);
-		
-		
-		
+	public void CreateChainInDBGoodTest() throws SQLException, IOException {
+		Connection con = connect();
+		PreparedStatement preparedStatement = null;
+		boolean res = new Chains(con).createChain("Yada", null, "Vandaly ind.");
+		assertTrue(res);
+		ResultSet rs = null;
+		preparedStatement = con.prepareStatement("SELECT * FROM groups where group_name='Yada'");
+		rs = preparedStatement.executeQuery();
+		assertTrue(rs.next());
+		String parent = rs.getString("parent_group_id");
+		assertNull(parent);
+		String type = rs.getString("group_type");
+		assertEquals("Vandaly ind.", type);
+
 	}
 
 }
